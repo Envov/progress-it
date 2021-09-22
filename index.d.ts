b7bd8c31dd5c34cd7db56819046339bc3540608c
UILUNP <uilkunp@gmail.com>
2021-09-22 20:48:39 +0800

    [#8] v1.1.0 build

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 declare type Effect = (percent: number, state: boolean | 'fail') => void;
 declare type Setter = (newState: boolean | 'fail') => Setter;
-declare type progress = (effect: Effect, option: Partial<IProgressPotions>) => (initState: boolean) => (newState: boolean) => Setter;
-interface IProgressPotions {
+declare type progress = (effect: Effect, option?: Partial<IProgressPotions>) => (initState: boolean) => (newState: boolean | 'fail') => Setter;
+declare type IProgressPotions = {
     percent: number;
     interval: number;
     speed: number;
     timeLine: number;
     timer: number | undefined;
-}
+};
 declare const progress: progress;
 export default progress;
